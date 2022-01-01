7736cef755fb483af51c30120daa90e4429a0753
AlexthePurplerose <96198775+AlexthePurplerose@users.noreply.github.com>
2022-01-02 00:23:36 +0800

    Update LTexture.h

--- finalproject/LTexture.h
+++ finalproject/LTexture.h
@@ -1,33 +1,47 @@
 #ifndef LTEXTURE_H
 #define LTEXTURE_H
+#include <SDL.h>
+#include <SDL_image.h>
 class LTexture
 {
-    public:
-        //Initializes variables
-        LTexture();
+	public:
+		//Initializes variables
+		LTexture();
+		//Deallocates memory
+		~LTexture();
 
-        //Deallocates memory
-        ~LTexture();
+		//Loads image at specified path
+		bool loadFromFile( std::string path );
+		bool loadTexture( std::string path );
+		#if defined(SDL_TTF_MAJOR_VERSION)
+		//Creates image from font string
+		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
+		#endif
 
-        //Loads image at specified path
-        bool loadFromFile( std::string path );
+		//Deallocates texture
+		void free();
 
-        //Deallocates texture
-        void free();
+		//Set color modulation
+		void setColor( Uint8 red, Uint8 green, Uint8 blue );
 
-        //Renders texture at given point
-        void render( int x, int y );
+		//Set blending
+		void setBlendMode( SDL_BlendMode blending );
 
-        //Gets image dimensions
-        int getWidth();
-        int getHeight();
+		//Set alpha modulation
+		void setAlpha( Uint8 alpha );
+		
+		//Renders texture at given point
+		void render( int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
 
-    private:
-        //The actual hardware texture
-        SDL_Texture* mTexture;
+		//Gets image dimensions
+		int getWidth();
+		int getHeight();
 
-        //Image dimensions
-        int mWidth;
-        int mHeight;
+		//Image dimensions
+		int mWidth;
+		int mHeight;
+	private:
+		//The actual hardware texture
+		SDL_Texture* mTexture;
 };
 #endif
