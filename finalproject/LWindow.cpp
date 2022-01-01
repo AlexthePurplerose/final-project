7da55fa6425f95682b610c8689f77b4f563b8255
AlexthePurplerose <96198775+AlexthePurplerose@users.noreply.github.com>
2022-01-02 00:28:31 +0800

    Update LWindow.cpp

--- finalproject/LWindow.cpp
+++ finalproject/LWindow.cpp
@@ -8,8 +8,6 @@ LWindow::LWindow()
 {
 	//Initialize non-existant window
 	mWindow = NULL;
-	mMouseFocus = false;
-	mKeyboardFocus = false;
 	mFullScreen = false;
 	mMinimized = false;
 	mWidth = 0;
@@ -22,8 +20,6 @@ bool LWindow::init()
 	mWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
 	if( mWindow != NULL )
 	{
-		mMouseFocus = true;
-		mKeyboardFocus = true;
 		mWidth = SCREEN_WIDTH;
 		mHeight = SCREEN_HEIGHT;
 	}
@@ -41,9 +37,6 @@ void LWindow::handleEvent( SDL_Event& e )
 	//Window event occured
 	if( e.type == SDL_WINDOWEVENT )
 	{
-		//Caption update flag
-		bool updateCaption = false;
-
 		switch( e.window.event )
 		{
 			//Get new dimensions and repaint on window size change
@@ -56,30 +49,6 @@ void LWindow::handleEvent( SDL_Event& e )
 			//Repaint on exposure
 			case SDL_WINDOWEVENT_EXPOSED:
 			SDL_RenderPresent( gRenderer );
-			break;
-
-			//Mouse entered window
-			case SDL_WINDOWEVENT_ENTER:
-			mMouseFocus = true;
-			updateCaption = true;
-			break;
-			
-			//Mouse left window
-			case SDL_WINDOWEVENT_LEAVE:
-			mMouseFocus = false;
-			updateCaption = true;
-			break;
-
-			//Window has keyboard focus
-			case SDL_WINDOWEVENT_FOCUS_GAINED:
-			mKeyboardFocus = true;
-			updateCaption = true;
-			break;
-
-			//Window lost keyboard focus
-			case SDL_WINDOWEVENT_FOCUS_LOST:
-			mKeyboardFocus = false;
-			updateCaption = true;
 			break;
 
 			//Window minimized
@@ -96,14 +65,6 @@ void LWindow::handleEvent( SDL_Event& e )
 			case SDL_WINDOWEVENT_RESTORED:
 			mMinimized = false;
             break;
-		}
-
-		//Update window caption with new data
-		if( updateCaption )
-		{
-			std::stringstream caption;
-			caption << "SDL Tutorial - MouseFocus:" << ( ( mMouseFocus ) ? "On" : "Off" ) << " KeyboardFocus:" << ( ( mKeyboardFocus ) ? "On" : "Off" );
-			SDL_SetWindowTitle( mWindow, caption.str().c_str() );
 		}
 	}
 	//Enter exit full screen on return key
@@ -129,9 +90,6 @@ void LWindow::free()
 	{
 		SDL_DestroyWindow( mWindow );
 	}
-
-	mMouseFocus = false;
-	mKeyboardFocus = false;
 	mWidth = 0;
 	mHeight = 0;
 }
@@ -144,16 +102,6 @@ int LWindow::getWidth()
 int LWindow::getHeight()
 {
 	return mHeight;
-}
-
-bool LWindow::hasMouseFocus()
-{
-	return mMouseFocus;
-}
-
-bool LWindow::hasKeyboardFocus()
-{
-	return mKeyboardFocus;
 }
 
 bool LWindow::isMinimized()
