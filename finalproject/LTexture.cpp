cd91c6e5972072df7977361c9930c2ae317bc97a
AlexthePurplerose <96198775+AlexthePurplerose@users.noreply.github.com>
2022-01-03 23:32:11 +0800

    Update LTexture.cpp

--- finalproject/LTexture.cpp
+++ finalproject/LTexture.cpp
@@ -117,6 +117,38 @@ void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* ce
 
 	SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
 }
+bool LTexture::loadTexture2( std::string path ,int height, int width)
+{
+	free();
+
+	SDL_Texture* newTexture = NULL;
+
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}
+	else
+	{
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+		if( newTexture == NULL )
+		{
+			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+		else
+		{
+			mWidth = width;
+			mHeight = height;
+		}
+
+		SDL_FreeSurface( loadedSurface );
+	}
+
+	mTexture = newTexture;
+	return mTexture != NULL;
+}
 
 int LTexture::getWidth()
 {
