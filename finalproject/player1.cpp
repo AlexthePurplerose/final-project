9e128f7e200ab3bd3dce0da525f52564c4938b2a
AlexthePurplerose <96198775+AlexthePurplerose@users.noreply.github.com>
2022-01-08 14:42:44 +0800

    Update player1.cpp

--- finalproject/player1.cpp
+++ finalproject/player1.cpp
@@ -7,47 +7,46 @@
 #include "player1.h"
 player1::player1()
 {
-    pos_stu_x = 0 ; //student's position x
-    pos_stu_y = 0 ; //student's position y
-    pos_note_x = 0 ; //note's position x
-    pos_note_y = 0 ; //note's position y
     ifcheat = false ;
     ifraise = false ;
 }
-void player1 :: handleEvent ( SDL_Event& e ) {
-	
-                if (e.type == SDL_KEYDOWN) {
-                    //Select surfaces based on key press
-                    switch (e.key.keysym.sym) {
-                        case SDLK_a:
+void player1::setpos()
+{
+	pos_stu_x = 0.3*gWindow.getWidth(); //student's position x
+    pos_stu_y = 0.4*gWindow.getHeight(); //student's position y
+    pos_note_x = 0.3*gWindow.getWidth(); //note's position x
+    pos_note_y = 0.55*gWindow.getHeight() ; //note's position y
+    pos_raise_x=0.3*gWindow.getWidth();
+    pos_raise_y=0.41*gWindow.getHeight();
+}
+void player1 :: handleEvent () {
+	const Uint8 *state=SDL_GetKeyboardState(NULL);
+                        if(state[SDL_SCANCODE_A])
+                        {
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_D].free();
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].free();
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_A].free();
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].loadFromFile( "./student.png" );
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_A].loadFromFile("./note.png" );
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_A].render(pos_note_x,pos_note_y);
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].render(pos_stu_x, pos_stu_y);
+                            gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].render(pos_stu_x,pos_stu_y);
                             ifcheat=1;
                             ifraise=0;
-                            break;
-                        case SDLK_d:
+                    	}
+                        else if(state[SDL_SCANCODE_D])
+                        {
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].free();
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_A].free();
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_D].free();
                             gKeyPressSurfaces[KEY_PRESS_SURFACE_D].loadFromFile( "./raise_hands.png" );
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_D].render(pos_stu_x,pos_stu_y);
+                            gKeyPressSurfaces[KEY_PRESS_SURFACE_D].render(pos_raise_x,pos_raise_y);
                             ifraise=1;
                             ifcheat=0;
-                            break;
-                        default:
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].free();
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_A].free();
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_D].free();
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].loadFromFile( "./student.png" );
-                            gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].render(pos_stu_x, pos_stu_y);
-                            ifraise=0;
-                            ifcheat=0;
-                            break;
-                    }
-                }
+                    	}
+                        else
+                        {
+                        	gKeyPressSurfaces[KEY_PRESS_SURFACE_A].free();
+	                        gKeyPressSurfaces[KEY_PRESS_SURFACE_D].free();
+	                        gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].loadFromFile( "./student.png" );
+	                        gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT1].render(pos_stu_x,pos_stu_y);
+	                        ifraise=0;
+	                        ifcheat=0;
+	                	}
 }
