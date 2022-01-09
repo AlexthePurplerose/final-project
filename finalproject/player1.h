0da72caa9ddf46eccd2eafbd59fb965b64fbd5cc
chiawen0104 <93430124+chiawen0104@users.noreply.github.com>
2022-01-09 13:22:17 +0800

    Add files via upload

--- finalproject/player1.h
+++ finalproject/player1.h
@@ -1,7 +1,14 @@
+//
+//  player1.hpp
+//  Final Project
+//
+//  Created by 盧家雯 on 2022/1/8.
+//
+
 #ifndef PLAYERONE
 #define PLAYERONE
 #include "Student.h"
-#include <SDL.h>
+#include <SDL2/SDL.h>
 class player1 : public Student
 {
     private:
@@ -10,8 +17,8 @@ class player1 : public Student
     int pos_stu_y ;
     int pos_note_x ;
     int pos_note_y ;
-    int pos_raise_x;
-    int pos_raise_y;
+    //int pos_raise_x;
+    //int pos_raise_y;
 
     protected:
 
